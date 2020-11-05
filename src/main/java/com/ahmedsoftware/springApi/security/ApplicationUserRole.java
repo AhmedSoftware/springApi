98a10caa479e27d27c87e3fc95543640df9032af
aali <aali@osaxis.fr>
2020-11-05 23:06:52 +0100

    Implement role and permission using guava librairie

---
+++ src/main/java/com/ahmedsoftware/springApi/security/ApplicationUserRole.java
@@ -0,0 +1,19 @@
+package com.ahmedsoftware.springApi.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.ahmedsoftware.springApi.security.ApplicatonUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet(COURSE_READ,STUDENT_READ,STUDENT_WRITE)),
+    ADMIN(Sets.newHashSet(COURSE_READ,COURSE_WRITE,STUDENT_READ,STUDENT_WRITE));
+    
+    
+    private final Set<ApplicatonUserPermission> permissions;
+    
+    ApplicationUserRole(Set<ApplicatonUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+}
---
+++ src/main/java/com/ahmedsoftware/springApi/security/ApplicatonUserPermission.java
@@ -0,0 +1,18 @@
+package com.ahmedsoftware.springApi.security;
+
+public enum ApplicatonUserPermission {
+    STUDENT_READ("student:read"),
+    STUDENT_WRITE("student:write"),
+    COURSE_READ("course:read"),
+    COURSE_WRITE("course:write");
+    
+    private final String permission;
+    
+    ApplicatonUserPermission(String permission) {
+        this.permission = permission;
+    }
+    
+    public String getPermission() {
+        return permission;
+    }
+}
