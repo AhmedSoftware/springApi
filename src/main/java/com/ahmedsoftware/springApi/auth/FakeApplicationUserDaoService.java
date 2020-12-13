ac505904ee22df56a9e20065ff6d6bd85e1ad925
aali <aali@osaxis.fr>
2020-12-13 18:35:47 +0100

    add aaplication user authentification user, userService, dao, daoService

---
+++ src/main/java/com/ahmedsoftware/springApi/auth/FakeApplicationUserDaoService.java
@@ -0,0 +1,60 @@
+package com.ahmedsoftware.springApi.auth;
+
+import com.ahmedsoftware.springApi.student.Student;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import com.ahmedsoftware.springApi.security.ApplicationUserRole;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.ahmedsoftware.springApi.security.ApplicationUserRole.*;
+
+@Repository("fake")
+public class FakeApplicationUserDaoService implements ApplicationUserDao{
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public FakeApplicationUserDaoService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
+        return getApplicationUsers()
+                .stream()
+                .filter(applicationUser -> username.equals(applicationUser.getUsername()))
+                .findFirst();
+    }
+
+    private List<ApplicationUser> getApplicationUsers(){
+        List<ApplicationUser> applicationUsers= Lists.newArrayList(
+                new ApplicationUser("azad",
+                        passwordEncoder.encode("password"),
+                        STUDENT.grantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true),
+                new ApplicationUser("ali",
+                        passwordEncoder.encode("password"),
+                        ADMIN.grantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true),
+                new ApplicationUser("galisse",
+                        passwordEncoder.encode("password"),
+                        ADMINTRAINEE.grantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true)
+        );
+        return applicationUsers;
+    }
+
+}
