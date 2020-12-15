7324739d26854bd945216d627772e2ea81098a1e
aali <aali@osaxis.fr>
2020-12-15 23:12:04 +0100

    add autorization

--- src/main/java/com/ahmedsoftware/springApi/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/ahmedsoftware/springApi/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -37,7 +37,8 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
                    authenticationRequest.getUsername(),
                    authenticationRequest.getPassword());
 
-           return authenticationManager.authenticate(authentication);
+           Authentication authenticate = authenticationManager.authenticate(authentication);
+           return authenticate;
 
        }catch (IOException ioException){
            throw new RuntimeException(ioException);
--- src/main/java/com/ahmedsoftware/springApi/security/ApplicationSecurityConfig.java
+++ src/main/java/com/ahmedsoftware/springApi/security/ApplicationSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.ahmedsoftware.springApi.security;
 
 import com.ahmedsoftware.springApi.auth.ApplicationUserService;
+import com.ahmedsoftware.springApi.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -43,31 +45,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
                 .authorizeRequests()
                 .antMatchers("/","index","/css/*","/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .defaultSuccessUrl("/courses",true)
-                    .usernameParameter("username")
-                    .passwordParameter("password")
-                .and()
-                .rememberMe()
-                    .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
-                    .key("secured")
-                    .rememberMeParameter("remember-me")
-                .and()
-                .logout()
-                    .logoutUrl("/logout")
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))
-                    .clearAuthentication(true)
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID","remember-me")
-                    .logoutSuccessUrl("/login");
+                .authenticated();
     }
 
     @Override
