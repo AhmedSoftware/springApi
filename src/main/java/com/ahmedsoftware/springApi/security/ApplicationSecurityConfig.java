09c5c31713c73b97f953cadb950ee21544730955
aali <aali@osaxis.fr>
2020-12-15 23:45:49 +0100

    JwtTokenVerifier authentication

--- src/main/java/com/ahmedsoftware/springApi/security/ApplicationSecurityConfig.java
+++ src/main/java/com/ahmedsoftware/springApi/security/ApplicationSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.ahmedsoftware.springApi.security;
 
 import com.ahmedsoftware.springApi.auth.ApplicationUserService;
+import com.ahmedsoftware.springApi.jwt.JwtTokenVerifier;
 import com.ahmedsoftware.springApi.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
